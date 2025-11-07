139aaf1bfa68b157e3c96f29562876c0c1460ac3
calistahappy3-a11y <calistahappy3@gmail.com>
2025-11-07 12:39:41 +0800

    Removed temp and functions files, update openshowall, rename P4_8-CMS.txt

--- openshowall.c
+++ openshowall.c
@@ -17,7 +17,13 @@ int student_count = 0;
 
 // Opens the database file and loads all student records into memory
 void openDatabase() {
-    FILE *file = fopen("TeamName-CMS.txt", "r");
+
+    char userinput[20];
+
+    printf("P4_8: ");
+    fgets(userinput, sizeof(userinput), stdin);
+
+    FILE *file = fopen("P4_8-CMS.txt", "r");
     char buffer[300]; // temporary storage for each line
     char temp_prog[100]; // temporary storage for programme and mark
 
@@ -50,7 +56,7 @@ void openDatabase() {
         }
     }
     
-    printf("The database file \"TeamName-CMS.txt\" is successfully opened.\n");
+    printf("CMS: The database file \"P4_8-CMS.txt\" is successfully opened.\n");
     fclose(file);
 }
 
@@ -95,7 +101,13 @@ void sort_by_mark(struct Student students[], int count){
 }
 
 void saveDatabase(struct Student students[], int count){
-    FILE *file = fopen("TeamName-CMS.txt", "w"); // open file for writing
+    
+    // char userinput3[20];
+
+    // printf("P4_8: ");
+    // fgets(userinput3, sizeof(userinput3), stdin);
+
+    FILE *file = fopen("P4_8-CMS.txt", "w"); // open file for writing
     if(file == NULL){
         printf("Error to open the file!\n");
         return;
@@ -110,48 +122,70 @@ void saveDatabase(struct Student students[], int count){
     }
 
     fclose(file);
-    printf("The database file \"TeamName-CMS.txt\" is successfully saved.\n");
+    printf("CMS: The database file \"P4_8-CMS.txt\" is successfully saved.\n");
 
 }
 
 int main() {
 
+    printf("--Declaration--\n");
+    printf("SIT's policy on copying does not allow the students to copy source code as well as assesment solutions from another person AI or other places. ");
+    printf("It is the students' responsibility to guarantee that their assessment solutions are their own work. ");
+    printf("Meanwhile, the students must also ensure that their work is not accessible by others. ");
+    printf("Where such plagiarism is detected, both of the assessments involved will receive ZERO mark.\n");
+    printf("We hereby declare that: \n");
+    printf(" - We fully understand and agree to the abovementioned plagiarism policy.\n");
+    printf(" - We did not copy any code from others or from other places.\n");
+    printf(" - We did not share our codes with others or upload to any other places for public access and will not do that in the future.\n");
+    printf(" - We agree that our project will receive Zero mark if there is any plagiarism detected.\n");
+    printf(" - We agree that we will not disclose any information or material of the group project to others or upload to any other places for public access.\n");
+    printf(" - We agree that we did not copy any code directly from AI generated sources.\n");
+    printf("Declared by: P4-8\n");
+    printf("Team members: \n");
+    printf(" 1. Andrew Gan\n");
+    printf(" 2. Aiko Mendoza Tan\n");
+    printf(" 3. Chong Xinhuei\n");
+    printf(" 4. Damien Teh\n");
+    printf(" 5. Happy Calista\n");
+    printf("Date: \n");
+    printf("\n");
+
     openDatabase(); // call function to open database file
 
     while(1) { 
-        char command[50];
-        printf("Enter command (SHOW ALL SORT BY ID/MARK, SHOW ALL, EXIT, SAVE): \n");
-        fgets(command, sizeof(command), stdin); // Get user input
-        command[strlen(command) - 1] = '\0';
+        char userinput2[30];
+        printf("P4_8: ");
+        fgets(userinput2, sizeof(userinput2), stdin); // Get user input for SHOW ALL
+        userinput2[strlen(userinput2) - 1] = '\0';
 
-        for(int i = 0; command[i] != '\0'; i++){
-            command[i] = toupper(command[i]); // convert to uppercase (for case insensitivity)
+        for(int i = 0; userinput2[i] != '\0'; i++){
+            userinput2[i] = toupper(userinput2[i]); // convert to uppercase (for case insensitivity)
         }
 
-        if(strstr(command, "SHOW ALL SORT BY ID") != NULL) { // strstr to check if substring exists
+        if(strstr(userinput2, "SHOW ALL SORT BY ID") != NULL) { // strstr to check if substring exists
             // Call sort by ID function
-            printf("Here are all the records found in the table \"StudentRecords\".\n");
+            printf("CMS: Here are all the records found in the table \"StudentRecords\".\n");
             sort_by_id(students, student_count);
             showAll(students, student_count);
         }
-        else if(strstr(command, "SHOW ALL SORT BY MARK") != NULL) {
+        else if(strstr(userinput2, "SHOW ALL SORT BY MARK") != NULL) {
             // Call sort by mark function
-            printf("Here are all the records found in the table \"StudentRecords\".\n");
+            printf("CMS: Here are all the records found in the table \"StudentRecords\".\n");
             sort_by_mark(students, student_count);
             showAll(students, student_count);
         }
-        else if(strcmp(command, "SHOW ALL") == 0){
-            printf("Here are all the records found in the table \"StudentRecords\".\n");
+        else if(strcmp(userinput2, "SHOW ALL") == 0){
+            printf("CMS: Here are all the records found in the table \"StudentRecords\".\n");
             showAll(students, student_count);
         }
-        else if(strcmp(command, "EXIT") == 0){
+        else if(strcmp(userinput2, "EXIT") == 0){
             break; // to exit the loop
         }
-        else if(strcmp(command, "SAVE") == 0){
+        else if(strcmp(userinput2, "SAVE") == 0){
             saveDatabase(students, student_count); // Save changes to the database file
         }
         else{
-            printf("Invalid command. Please try again.\n");
+            printf("Invalid input. Please try again.\n");
         }
     }
 
