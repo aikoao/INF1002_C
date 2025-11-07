00e076d2050b609fe783af5bfbef5d93484dbe12
calistahappy3-a11y <calistahappy3@gmail.com>
2025-11-07 23:37:13 +0800

    Add new structure (main.c, database.c, database.h) and remove old (openshowall) file.

---
+++ database.h
@@ -0,0 +1,32 @@
+#ifndef DATABASE_H // checks the DATABASE_H if it has already been defined
+#define DATABASE_H
+
+#define MAX_RECORDS 200
+#define NAME_LENGTH 50
+#define PROGRAMME_LENGTH 100
+
+// Define the structure for student records
+typedef struct {
+    int id;
+    char name[NAME_LENGTH];
+    char programme[PROGRAMME_LENGTH];
+    float mark;
+}Student;
+
+extern Student students[MAX_RECORDS]; // global array to store the student records
+extern int student_count; // global variable to mark the current number of students 
+
+// File operations 
+void openDatabase(void);
+void showAll(void);
+void saveDatabase();
+void sort_by_id(void);
+void sort_by_mark(void);
+
+// Command functions (Team)
+void update_record();      // Aiko
+void query_record();       // Andrew  
+void insert_record();      // Xin Huei
+void delete_record();      // Damien
+
+#endif // end of the DATABASE_H
