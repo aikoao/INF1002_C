4c65fb8b2ec40d2d968c2e96d21991387840bd4d
andrewgan <97227125+godcoder6000@users.noreply.github.com>
2025-11-12 17:24:13 +0800

    fixed the id issue where the number was too big to show

--- query.c
+++ query.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "query.h"
 
 void queryStudentByID(Student students[], int student_count) {
-    int queryId, found = 0;
-    printf("Enter the student ID to search: ");
-    scanf("%d", &queryId);
-    getchar();
+    char idInput[100];
+    int queryId, found = 0, valid = 0;
+    while (!valid) {
+        printf("Enter the student ID to search (7 digits): ");
+        fgets(idInput, sizeof(idInput), stdin);
+        idInput[strcspn(idInput, "\n")] = 0; // Remove newline
+
+        int len = strlen(idInput);
+        valid = 1;
+        if (len != 7) valid = 0;
+        for (int i = 0; i < len && valid; i++) {
+            if (!isdigit((unsigned char)idInput[i])) valid = 0;
+        }
+        if (!valid)
+            printf("Please input a proper ID (2xxxxxx)\n");
+    }
+    sscanf(idInput, "%d", &queryId);
 
     for (int i = 0; i < student_count; i++) {
         if (students[i].id == queryId) {
             printf("Record Found:\n");
             printf("%-10s %-22s %-26s %-8s\n", "ID", "Name", "Programme", "Mark");
             printf("%-10d %-22s %-26s %-8.2f\n",
-                   students[i].id, students[i].name, students[i].programme, students[i].mark);
+                students[i].id, students[i].name, students[i].programme, students[i].mark);
             found = 1;
             break;
         }
